3700298042659755ce611109b3b2780e99f33680
TarenShaw <tshaaaaw@gmail.com>
2021-10-04 22:34:49 +0200

    Update

--- Employee_Turnover_Script.R
+++ Employee_Turnover_Script.R
@@ -1,51 +1,82 @@
-#Load Packages
-pacman::p_load(tidyverse,readr, lubridate, skimr)
+# Load Packages
+pacman::p_load(tidyverse, readr, lubridate, skimr, styler)
 
-#Load Data
+# Load Data
 df <- readr::read_csv("HRDataset_v14.csv")
 
-#Summary or Data
+# Summary or Data
 skim(df)
 
-#Transform Data
-df <- df %>% mutate(`DateofTermination` = mdy(df$`DateofTermination`), 
-                    `DateofHire` = mdy(df$`DateofHire`), 
-                    Md_termination = format(`DateofTermination`,'%Y-%m'))
+# Transform Data
+df <- df %>% mutate(
+  DateofTermination = mdy(DateofTermination),
+  DateofHire = mdy(DateofHire)
+)
 
-#How many leavers per year
-df %>% mutate(Year_termination = year(df$`DateofTermination`)) %>% 
+
+
+# How many leavers per year
+df %>%
+  mutate(Year_termination = year(df$`DateofTermination`)) %>%
   count(Year_termination) %>%
-  drop_na(Year_termination) %>% 
-  mutate(Year_termination =factor(Year_termination)) %>% 
-  mutate(Year_termination = fct_reorder(Year_termination, n)) %>% 
-  ggplot(aes(x =Year_termination, y =n)) + geom_col(fill = "#0072b1") +
-  geom_text(aes(label = n), hjust = -0.2) + 
-  coord_flip() + theme_bw() 
+  drop_na(Year_termination) %>%
+  mutate(Year_termination = factor(Year_termination)) %>%
+  mutate(Year_termination = fct_reorder(Year_termination, n)) %>%
+  ggplot(aes(x = Year_termination, y = n)) +
+  geom_col(fill = "#0072b1") +
+  geom_text(aes(label = n), hjust = -0.2) +
+  coord_flip() +
+  theme_bw()
 
-#What is the mean number of leavers - some extreme outliers
-median_terminations <- df %>% mutate(Year_termination = year(df$`DateofTermination`)) %>% 
-  count(Year_termination) %>% #We omit 2010 since it only has two observations
-  drop_na(Year_termination) %>% 
-  summarise(mean = median(n)) %>%  
-  as.numeric() %>% print()
-
-#compare average leavers to leavers per year
-df %>% mutate(Year_termination = year(df$`DateofTermination`)) %>% 
+# What is the mean number of leavers - some extreme outliers
+median_terminations <- df %>%
+  mutate(Year_termination = year(df$`DateofTermination`)) %>%
   count(Year_termination) %>%
-  drop_na(Year_termination) %>% 
-  ggplot(aes(x =Year_termination, y =n)) + geom_line() + geom_point() +
-  geom_text(aes(label = n), vjust = -0.5) + 
-  theme_bw() + geom_hline(yintercept= median_terminations, linetype="dashed", color = "red")
+  drop_na(Year_termination) %>%
+  summarise(mean = median(n)) %>%
+  as.numeric() %>%
+  print()
 
-#Write a function for avg employees 2
-avg_emp_year <- function(year = "2010") {
-  
-  hire_year <- paste(year, "-01-01", sep = "")
-  term_year <- paste(year, "-12-31", sep = "")
-  
-  (df %>% select(DateofHire, DateofTermination) %>% 
-     filter(df$DateofHire < hire_year | DateofTermination < term_year) %>% 
-     nrow())/2
-  
+# compare average leavers to leavers per year
+df %>%
+  mutate(Year_termination = year(df$`DateofTermination`)) %>%
+  count(Year_termination) %>%
+  drop_na(Year_termination) %>%
+  ggplot(aes(x = Year_termination, y = n)) +
+  geom_line() +
+  geom_point() +
+  geom_text(aes(label = n), vjust = -0.5) +
+  theme_bw() +
+  geom_hline(yintercept = median_terminations, linetype = "dashed", color = "red")
+
+# number of emplyees at beginnning of year
+n_emp_year_start <- function(year = "2010") {
+  fun_year <- paste(year, "-01-01", sep = "")
+
+  df %>%
+    select(DateofHire, DateofTermination) %>%
+    filter(
+      DateofHire <= fun_year,
+      DateofTermination > fun_year | is.na(DateofTermination)
+    ) %>%
+    nrow()
 }
+
+n_emp_year_start()
+
+# number of employees at end of year
+n_emp_year_end <- function(year = "2010") {
+  year_num_plus <- as.character(as.numeric(year) + 1)
+  fun_year <- paste(year_num_plus, "-01-01", sep = "")
+
+  df %>%
+    select(DateofHire, DateofTermination) %>%
+    filter(
+      DateofHire <= fun_year,
+      DateofTermination > fun_year | is.na(DateofTermination)
+    ) %>%
+    nrow()
+}
+
+n_emp_year_end()
 
