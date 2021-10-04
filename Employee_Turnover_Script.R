55371c757bbc62e64f292b1f47f236f94361ddad
TarenShaw <tshaaaaw@gmail.com>
2021-10-04 15:47:16 +0200

    Add comments and Write formula for avg emplyee year

--- Employee_Turnover_Script.R
+++ Employee_Turnover_Script.R
@@ -1,13 +1,16 @@
-pacman::p_load(tidyverse,readr, lubridate)
+#Load Packages
+pacman::p_load(tidyverse,readr, lubridate, skimr)
 
+#Load Data
 df <- readr::read_csv("HRDataset_v14.csv")
 
-skimr::skim(df)
+#Summary or Data
+skim(df)
 
+#Transform Data
 df <- df %>% mutate(`DateofTermination` = mdy(df$`DateofTermination`), 
                     `DateofHire` = mdy(df$`DateofHire`), 
                     Md_termination = format(`DateofTermination`,'%Y-%m'))
-
 
 #How many leavers per year
 df %>% mutate(Year_termination = year(df$`DateofTermination`)) %>% 
@@ -19,25 +22,31 @@ df %>% mutate(Year_termination = year(df$`DateofTermination`)) %>%
   geom_text(aes(label = n), hjust = -0.2) + 
   coord_flip() + theme_bw() 
 
-#What is the mean number of leavers 
-avg_terminations <- df %>% mutate(Year_termination = year(df$`DateofTermination`)) %>% 
+#What is the mean number of leavers - some extreme outliers
+median_terminations <- df %>% mutate(Year_termination = year(df$`DateofTermination`)) %>% 
   count(Year_termination) %>% #We omit 2010 since it only has two observations
   drop_na(Year_termination) %>% 
-  summarise(mean = mean(n)) %>%  
+  summarise(mean = median(n)) %>%  
   as.numeric() %>% print()
 
 #compare average leavers to leavers per year
 df %>% mutate(Year_termination = year(df$`DateofTermination`)) %>% 
   count(Year_termination) %>%
   drop_na(Year_termination) %>% 
-  ggplot(aes(x =Year_termination, y =n)) + geom_col(fill = "#0072b1") +
-  geom_text(aes(label = n), vjust = -0.2) + 
-  theme_bw() + geom_hline(yintercept= avg_terminations, linetype="dashed", color = "red")
+  ggplot(aes(x =Year_termination, y =n)) + geom_line() + geom_point() +
+  geom_text(aes(label = n), vjust = -0.5) + 
+  theme_bw() + geom_hline(yintercept= median_terminations, linetype="dashed", color = "red")
 
+#Write a function for avg employees
+avg_emp_year <- function(Hire_date = "2010-01-01", Term_date= "2010-12-31") {
+  
+  (df %>% select(DateofHire, DateofTermination) %>% 
+    filter(df$DateofHire < Hire_date | DateofTermination < Term_date) %>% 
+    nrow())/2
 
+}
 
-
-
+employees_year(Hire_date = "2011-01-01", Term_date = "2011-12-31")
 
 
 
